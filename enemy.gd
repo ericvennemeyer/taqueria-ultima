75e87ff1bab6ed60f72c0ebe21a122cb45e5c1b7
Eric Vennemeyer <82679637+ericvennemeyer@users.noreply.github.com>
2024-05-25 19:45:06 -0700

    Create custom input actions. Create basic system for switching between muliple player characters.

--- enemy.gd
+++ enemy.gd
@@ -1,7 +1,6 @@
 class_name Enemy
 extends CharacterBody2D
 
-
 const SPEED = 200.0
 const JUMP_VELOCITY = -400.0
 
--- world.gd
+++ world.gd
@@ -1,11 +1,39 @@
 extends Node2D
 
+var player_characters_array: Array[Node]
+var current_player
+
+
+@onready var player_controlled: Node = $"Player-Controlled"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	RenderingServer.set_default_clear_color(Color.BLACK)
+	
+	player_characters_array = player_controlled.get_children()
+	for player in player_characters_array:
+		if player.is_active:
+			current_player = player
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("change_player"):
+		change_player_character()
+
+
+func change_player_character():
+	var next_player_index
+	var current_player_index = player_characters_array.find(current_player)
+	if current_player_index != -1:
+		next_player_index = current_player_index + 1
+		if next_player_index > player_characters_array.size() - 1:
+			next_player_index = 0
+	print(current_player_index)
+	print(next_player_index)
+	current_player = player_characters_array[next_player_index]
+	for player in player_characters_array:
+		player.is_active = false
+		player.selection_indicator.visible = false
+	current_player.is_active = true
+	current_player.selection_indicator.visible = true
