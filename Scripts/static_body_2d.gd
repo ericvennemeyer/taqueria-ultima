80bb0c6f55fd9e52caa850a0d7b0bd0a8bf962b1
Eric Vennemeyer <82679637+ericvennemeyer@users.noreply.github.com>
2024-05-28 17:54:58 -0700

    Create default player movement data resource. Add in movement code from Heartbeast platformer tutorial and modify to suit project.

--- Scripts/static_body_2d.gd
+++
@@ -1,8 +0,0 @@
-extends StaticBody2D
-
-@onready var collision_polygon_2d: CollisionPolygon2D = $CollisionPolygon2D
-@onready var polygon_2d: Polygon2D = $CollisionPolygon2D/Polygon2D
-
-
-func _ready() -> void:
-	polygon_2d.polygon = collision_polygon_2d.polygon
