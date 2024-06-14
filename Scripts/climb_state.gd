4dcfa2bec24ff8d3d166ccc3360d18a97cab1017
Eric Vennemeyer <82679637+ericvennemeyer@users.noreply.github.com>
2024-06-14 11:19:30 -0700

    Add empty ClimbState, HurtState and DeathStates to Player CharacterStateMachine

---
+++ Scripts/climb_state.gd
@@ -0,0 +1,22 @@
+class_name ClimbState
+extends State
+
+
+func state_physics_process(delta):
+	pass
+
+
+func state_input(event: InputEvent):
+	pass
+
+
+func state_on_animation_finished(anim_name: String):
+	pass
+
+
+func on_enter(prev_state: State):
+	pass
+
+
+func on_exit():
+	pass
