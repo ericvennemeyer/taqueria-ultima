25311cfebb04f1a75c55e43edff3ecee5f6dc2d2
Eric Vennemeyer <82679637+ericvennemeyer@users.noreply.github.com>
2024-06-14 10:59:37 -0700

    Add export vars to GroundState for applicable animations and next_state. Create a test level with enemies, music, and other characters removed. Turn off autoplay on MenuMusic

--- Scripts/ground_state.gd
+++ Scripts/ground_state.gd
@@ -1,6 +1,13 @@
 class_name GroundState
 extends State
 
+@export var idle_animation: String = "idle"
+@export var idle_attack_animation: String = "idle_attack"
+@export var run_animation: String = "run"
+@export var run_attack_animation: String = "run_attack"
+
+@export var jump_state: State
+
 
 func state_physics_process(delta):
 	pass
@@ -15,8 +22,7 @@ func state_on_animation_finished(anim_name: String):
 
 
 func on_enter(prev_state: State):
-	pass
-	#animation_player.play()
+	animation_player.play(idle_animation)
 
 
 func on_exit():
