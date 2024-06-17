8b4a7a715186267617d6bba27979d9ba79e50342
Eric Vennemeyer <82679637+ericvennemeyer@users.noreply.github.com>
2024-06-17 11:43:48 -0700

    Create settings menu screen with sliders for Master, Music, and SFX volume.

---
+++ Scripts/settings.gd
@@ -0,0 +1,11 @@
+extends CanvasLayer
+
+@onready var back_button: Button = $CenterContainer/VBoxContainer/BackButton
+
+
+func _ready() -> void:
+	RenderingServer.set_default_clear_color(Color.BLACK)
+
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/start.tscn")
